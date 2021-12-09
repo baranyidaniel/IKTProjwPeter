f308ef4587f1a0576f5085339043e0d1f731e492
baranyidanee <baranyidaniel@turr.hu>
2021-12-09 09:36:38 +0100

    kigyo

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from os import system
 system("cls")
 import csv
+
+#beolvas
+
 file = open("otos.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
@@ -10,8 +13,21 @@ for row in csvreader:
     rows.append(row)
 file.close()
 
+#nyeroszamok lista
+
+nyeroszamok = []
+
+for i in range(len(rows)):
+    nyeroszamok.append(str(rows[i].split(";")[-5:]))
 #multheti számok
 
 multheti1 = str(rows[0]).split(";")
-multheti1 = str(rows[0]).split(";")
-print(f"A múlt heti nyerőszámok: {multheti[-5:]}")
+multheti2 = str(rows[1]).split(";")
+print(f"A múlt heti nyerőszámok: {multheti1[-5:]}")
+print(f"Azelőtti heti nyerőszámok: {multheti2[-5:]}")
+
+#leggyakoribb számok
+
+print(most_frequent(nyeroszamok))
+
+#legkisebb
