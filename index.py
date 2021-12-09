2dd5e57ca3bae63ba35947dc909a3a02341fa2eb
baranyidanee <baranyidaniel@turr.hu>
2021-12-09 10:19:26 +0100

    hehekigyo

--- index.py
+++ index.py
@@ -13,12 +13,9 @@ for row in csvreader:
     rows.append(row)
 file.close()
 
-#nyeroszamok lista
-
 nyeroszamok = []
+for i in range(len)
 
-for i in range(len(rows)):
-    nyeroszamok.append(str(rows[i].split(";")[-5:]))
 #multheti számok
 
 multheti1 = str(rows[0]).split(";")
@@ -28,6 +25,6 @@ print(f"Azelőtti heti nyerőszámok: {multheti2[-5:]}")
 
 #leggyakoribb számok
 
-print(most_frequent(nyeroszamok))
+
 
 #legkisebb
